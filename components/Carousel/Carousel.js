9993e378c58613850abd0c0771867a47781fa427
vospader13 <52944109+vospader13@users.noreply.github.com>
2019-12-13 14:18:42 -0700

    Working on stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,77 @@
     <div class="right-button"> > </div>
   </div>
 */
+let cc = document.createElement('div');
+cc.classList.add('carousel');
+let c = document.querySelector('.carousel-container');
+c.appendChild(cc);
+c.style.width = '100%';
+c.style.height = '500px';
+cc.style.zIndex = '3';
+let img = document.createElement('img');
+
+let previous,next,currentSlide;
+window.onload = () =>{
+let build = () => {
+
+  let creator = (ele, attributes, ...children) => {
+    const el = document.createElement(ele)
+
+    for (key in attributes) {
+      el.setAttribute(key, attributes[key])
+    }
+
+    children.forEach(child => {
+      if (typeof child === 'string') {
+        el.appendChild(document.createTextNode(child))
+      } else {
+        el.appendChild(child)
+      }
+    })
+    return el;
+
+}
+
+let carousel = creator('div',{class:'carousel'},previous = creator('div',{class:'left-button'},`<`),
+next = creator('div',{class:'right-button'},`>`),creator('img',{class:'slide',src:'./assets/carousel/mountains.jpeg'}),
+creator('img',{class:'slide',src:'./assets/carousel/computer.jpeg'}),creator('img',{class:'slide',src:'./assets/carousel/trees.jpeg'}),
+creator('img',{class:'slide',src:'./assets/carousel/turntable.jpeg'}));
+let slides = document.querySelectorAll('.slide');
+console.log(slides)
+// const slides = document.querySelectorAll('.slide');
+cc.appendChild(carousel);
+slides.forEach(function(s){
+  s.style.zIndex = '5';
+
+  carousel.appendChild(s)
+})
+
+function nextSlide(){
+	goToSlide(currentSlide+1);
+}
+
+
+function previousSlide(){
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+
+	slides[currentSlide].className = 'slide';
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = 'slide';
+}
+
+
+next.onclick = function(){
+	nextSlide();
+};
+previous.onclick = function(){
+
+	previousSlide();
+};
+}
+
+build()
+}
+
